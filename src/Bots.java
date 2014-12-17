cca72c2cac7ae63366b14f11552910079896e8c8
Tharinda Ehelepola <tharinda221@gmail.com>
2014-12-17 15:03:22 +0000

    using a thread

--- src/Bots.java
+++ src/Bots.java
@@ -49,49 +49,9 @@ public class Bots {
                         //get Subnet Mask
                         String subip=telnet.AutomatedTelnet("route-server.ip-plus.net", "rviews", "rviews", ip);
 
-                        //Identify a Subnet bots and add that subnet into Hashmap
-                        if(Node.subNetIn(Submask, subip) && !Node.searchNode(IPs,ip)  ){
-                            if(Node.searchNode(SubIPs, subip)){
-                                Node.UpdateNode(SubIPs, subip, 1);
-                            }
-                            else{
-                                SubIPs.put(subip, new Node(subip, 2, time));
-                            }
-                        }
-                        //Identify a bot and add it into Hashmap
-                        if(Node.searchNode(IPs, ip)){
-                            Node.UpdateNode(IPs, ip, 1);
+                        BotsMain BotsMain=new BotsMain(ip,time,subip);
+                        BotsMain.start();
 
-
-                        }
-                        else{
-                            IPs.put(ip, new Node(ip, 1, time));
-                            Submask.add(subip);
-
-                        }
-
-
-                        //Blocked if counts is equal to some specific value
-                        if(Node.SelectIpAdd(IPs, ip,2)){
-
-                            Node.writeIPS(ip,time,".\\Blocked.txt");
-                            new BlockIP(ip,"ns",ip);
-                            t = executor.schedule(new UNBlockIP(ip,"ns"), 10, SECONDS);
-                            IPs.remove(ip);
-
-                            Submask.remove(subip);
-
-                        }
-
-                        //Blocked if Subnet counts is equal to some specific value Block it and Unblock it earlier that a IP
-
-                        if(Node.SelectIpAdd(SubIPs, subip,5)){
-
-                            Node.writeIPS(subip,time,".\\BlockedSubs.txt");
-                            new BlockIP(ip,"s",subip);
-                            t = executor.schedule(new UNBlockIP(subip,"s"), 2, SECONDS);
-                            SubIPs.remove(subip);
-                        }
 
 
                     }
